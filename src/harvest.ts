43f204904192da9089ce57aa348aa02147a9858d
Peter Duchovni <caufeminecraft+github@gmail.com>
2023-07-28 22:48:06 -0400

    add seasons mode to harvest

--- src/harvest.ts
+++ src/harvest.ts
@@ -1,20 +1,53 @@
 import { NS } from "@ns";
 import { nodes } from 'global';
 import { canHack } from 'helper';
+import { hackOnce } from "./rip";
 
-export async function skim(ns: NS): Promise<void> {
+export async function seasons(ns: NS): Promise<void> {
+    for (const node of nodes) {
+        if (canHack(ns, node.name)) {
+            hackOnce(ns, node.name);
+        }
+    }
+}
+
+export async function reap(ns: NS): Promise<number> {
+    let accum = 0;
     for (const node of nodes) {
         if (canHack(ns, node.name)) {
             if (ns.getServerMoneyAvailable(node.name) > 0) {
-                await ns.hack(node.name);
+                accum += (await ns.hack(node.name));
             }
+        }
+    }
+    return accum;
+}
+
+export async function replant(ns: NS): Promise<void> {
+    for (const node of nodes) {
+        if (canHack(ns, node.name)) {
+            await ns.grow(node.name);
+            await ns.weaken(node.name);
         }
     }
 }
 
 export async function main(ns: NS): Promise<void> {
-    for (; ;) {
-        await skim(ns);
-        await ns.sleep(60000);
+    switch (ns.args[0] ?? "reap") {
+        case "reap": {
+            const totalHarvest = await reap(ns);
+            ns.toast(`Harvested $${totalHarvest} in total`, "success", 5000);
+            return;
+        }
+        case "replant": {
+            for (; ;) {
+                await replant(ns);
+            }
+        }
+        case "seasons": {
+            for (; ;) {
+                await seasons(ns);
+            }
+        }
     }
 }
