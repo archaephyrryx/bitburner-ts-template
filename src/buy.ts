73a55319db95a3f1c0577199bc5f20e556530d93
Peter Duchovni <caufeminecraft+github@gmail.com>
2024-03-07 12:27:51 +1100

    buy.ts: use bitnode multipliers 4s scale, support ram/cores

--- src/buy.ts
+++ src/buy.ts
@@ -1,7 +1,7 @@
 import { AutocompleteData, NS } from "@ns";
 import { BUDGET, makePurchase } from "./budget";
 
-const cmds = ['tor', 'wse', 'tix-api', '4s', '4s-api'];
+const cmds = ['tor', 'wse', 'tix-api', '4s', '4s-api', 'home-ram', 'home-core'] as const;
 // TODO: check costs once on new bitnode
 const TORCOST = 200_000;
 const WSECOST = 200_000_000;
@@ -11,32 +11,44 @@ const API4SCOST = 25_000_000_000;
 
 async function buyTor(ns: NS): Promise<boolean> {
     const f = (async (ns: NS) => { return ns.singularity.purchaseTor(); });
-    const id = await BUDGET.request(ns, TORCOST);
+    const id = BUDGET.request(ns, TORCOST, "purchase TOR router");
     return makePurchase(ns, id, f);
 }
 
 async function buyWSE(ns: NS): Promise<boolean> {
     const f = (async (ns: NS) => { return ns.stock.purchaseWseAccount(); });
-    const id = await BUDGET.request(ns, WSECOST);
+    const id = BUDGET.request(ns, WSECOST, "purchase WSE Account");
+    return makePurchase(ns, id, f);
+}
+
+async function buyHomeRam(ns: NS): Promise<boolean> {
+    const f = (async (ns: NS) => { return ns.singularity.upgradeHomeRam(); });
+    const id = BUDGET.request(ns, ns.singularity.getUpgradeHomeRamCost(), "upgrade home RAM");
+    return makePurchase(ns, id, f);
+}
+
+async function buyHomeCore(ns: NS): Promise<boolean> {
+    const f = (async (ns: NS) => { return ns.singularity.upgradeHomeCores(); });
+    const id = BUDGET.request(ns, ns.singularity.getUpgradeHomeCoresCost(), "upgrade home cores");
     return makePurchase(ns, id, f);
 }
 
 
 async function buyTIX(ns: NS): Promise<boolean> {
     const f = (async (ns: NS) => { return ns.stock.purchaseTixApi(); });
-    const id = await BUDGET.request(ns, TIXCOST);
+    const id = BUDGET.request(ns, TIXCOST, "purchase TIX API");
     return makePurchase(ns, id, f);
 }
 
 async function buy4SD(ns: NS): Promise<boolean> {
     const f = (async (ns: NS) => { return ns.stock.purchase4SMarketData(); });
-    const id = await BUDGET.request(ns, DATA4SCOST);
+    const id = BUDGET.request(ns, DATA4SCOST * ns.getBitNodeMultipliers().FourSigmaMarketDataCost, "purchase 4S data");
     return makePurchase(ns, id, f);
 }
 
 async function buy4SApi(ns: NS): Promise<boolean> {
     const f = (async (ns: NS) => { return ns.stock.purchase4SMarketDataTixApi(); });
-    const id = await BUDGET.request(ns, API4SCOST);
+    const id = BUDGET.request(ns, API4SCOST * ns.getBitNodeMultipliers().FourSigmaMarketDataApiCost, "purchase 4S API");
     return makePurchase(ns, id, f);
 }
 
@@ -49,14 +61,14 @@ function announce(ns: NS, what: string, success: boolean) {
 }
 
 export async function main(ns: NS) {
-    switch (ns.args[0]) {
+    switch (ns.args[0] as typeof cmds[number]) {
         case 'tor':
             announce(ns, "TOR Router", await buyTor(ns));
             return;
         case 'wse':
             announce(ns, "WSE Account", await buyWSE(ns));
             return;
-        case 'tix':
+        case 'tix-api':
             announce(ns, "TIX API Access", await buyTIX(ns));
             return;
         case '4s':
@@ -64,6 +76,12 @@ export async function main(ns: NS) {
             return;
         case '4s-api':
             announce(ns, "4S Market Data TIX API", await buy4SApi(ns));
+            return;
+        case 'home-ram':
+            announce(ns, `Home Ram Upgrade (==>${ns.formatRam(ns.getServerMaxRam("home") * 2)})`, await buyHomeRam(ns));
+            return;
+        case 'home-core':
+            announce(ns, `Home Core Upgrade (==>${ns.getServer("home").cpuCores + 1})`, await buyHomeCore(ns));
             return;
         default:
             ns.tprint(`WARN Usage: ${ns.getScriptName()} [${cmds.join(' | ')}]`);
